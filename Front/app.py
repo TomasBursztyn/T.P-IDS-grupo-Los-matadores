70c4be69ed573a8a48976600472eda8e4a020b53
Axel C. Lopez <lopezaxel@protonmail.com>
2024-05-30 21:35:03 -0300

    Install flask package, add instructions to run front
    Instalar el paquete flask en las instrucciones de pipenv.
    Añadir instrucciones para correr el front

--- Front/app.py
+++ Front/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 
-PORT = 8080
+PORT = 5000
 
 app = Flask(__name__)
 
@@ -9,4 +9,4 @@ def index():
     return render_template("index.html")
 
 if __name__ == '__main__':
-    app.run(debug=True, port=PORT)
+    app.run(host="127.0.0.1", port=PORT, debug=True)
---
+++ install.sh
@@ -0,0 +1,19 @@
+# instrucciones para correr el front (un boceto inicial)
+
+FRONT_FOLDER=./Front
+
+# instala pipenv por si acaso
+pip install pipenv --user
+
+# se mueve a la carpeta front
+cd FRONT_FOLDER
+
+# instalo las dependencias del proyecto administrado por pipenv
+pipenv install
+
+# entro a la shell del proyecto
+pipenv shell
+
+# corro el front con el --debug activado para actualizar los cambios
+# ojo esto solo para desarrollo (no meter en produccion con --debug activado)
+pipenv run flask run --debug
