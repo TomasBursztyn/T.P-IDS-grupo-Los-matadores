c6e41c33ab06ed374b36830690d16a7a27077909
jorgeha <jhuallpa@fi.uba.ar>
2024-06-01 22:36:44 -0300

    base.html agregado y rutas a vistas template

--- Front/app.py
+++ Front/app.py
@@ -8,5 +8,21 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/blog")
+def blog():
+    return render_template("blog.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/hotel")
+def hotel():
+    return render_template("hotel.html")
+
+@app.route("/services")
+def services():
+    return render_template("services.html")
+
 if __name__ == '__main__':
     app.run(host="127.0.0.1", port=PORT, debug=True)
