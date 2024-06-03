c03efa0c0864f35445f804bcc9ab5304da0c8ea7
sgarciat <sgarciat@fi.uba.ar>
2024-06-03 19:16:44 -0300

    Agrego el form de contacto, creo su js y lo aplico #31

---
+++ Front/static/js/contacto.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var btnContacto = document.getElementById("enviar-contacto");
+
+    btnContacto.addEventListener("click", function(event) {
+        event.preventDefault(); // Prevenir el envío del formulario
+
+        // Aquí podrías agregar el código para enviar los datos del formulario al servidor si es necesario
+
+        // Mostrar un mensaje de confirmación
+        alert("Formulario Enviado Correctamente");
+    });
+});
