92b70351797b8dbff2cab91a28de9934bff7f431
ValentinProvostFIUBA <vprovost@fi.uba.ar>
2024-06-03 10:05:14 -0300

    creacion de los errorhandler #26

--- Front/app.py
+++ Front/app.py
@@ -24,5 +24,14 @@ def hotel():
 def services():
     return render_template("services.html")
 
+
+@app.errorhandler(404)  
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def server_error(e):
+    return render_template('500.html'), 500
+
 if __name__ == '__main__':
     app.run(host="127.0.0.1", port=PORT, debug=True)
