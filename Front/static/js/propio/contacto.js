d534e341fcb024bc85dd8eaa8f1467769f5ae6c9
Axel C. Lopez <lopezaxel@protonmail.com>
2024-06-04 17:31:56 -0300

    Separar los archivos de JS de los nuestros, a los de la plantilla
    Me parece que se pueden mejorar los nombres de las carpetas todavia

--- Front/static/js/propio/contacto.js
+++ Front/static/js/propio/contacto.js
@@ -1,6 +1,7 @@
 document.addEventListener("load", function () {
     const btnContacto = document.getElementById("btn_enviar_contacto");
 
+    // Evento que se ejecuta cuando se clickea el boton con id de "btn_enviar_contacto"
     btnContacto.addEventListener("click", function (event) {
         event.preventDefault(); // Prevenir el envío del formulario
 
--- Front/static/js/propio/reservar.js
+++ Front/static/js/propio/reservar.js
@@ -1,10 +1,7 @@
-// usar "load" en vez de "DOMContentLoaded" porque "load" espera que se cargue
-// completamente el HTML y los recursos externos.
-// https://javascript.info/onload-ondomcontentloaded
 document.addEventListener("load", function () {
     const btnReservar = document.getElementById("btn_reservar");
 
-    // Evento que se ejecuta cuando se clickea el boton con id de "reservar"
+    // Evento que se ejecuta cuando se clickea el boton con id de "btn_reservar"
     btnReservar.addEventListener("click", function (event) {
         event.preventDefault(); // Prevenir el envío del formulario
 
