127eba90bc7899efccd9cfea47a8fb6b0f92deda
facundo <lpintol@fi.uba.ar>
2024-06-04 22:40:58 +0000

    Conecte el form de contact.html al app.py

--- Front/app.py
+++ Front/app.py
@@ -8,5 +8,72 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/blog")
+def blog():
+    return render_template("blog.html")
+
+#aca agrego un if la cual funciona si la peticion es del tipo post guarda en tres variables la informacion respectiva para luego imprimirlas y si la peticion es del tipo get te envia a la ruta de contact
+
+
+@app.route ("/contact" , methods = ["GET", "POST"])
+def contact():
+    if request.method == "POST":
+        nombre = request.form.get ("contacto_nombre")
+        email = request.form.get ("contacto_email")
+        message = request.form.get ("contacto_mensaje")
+
+#aca agrege un print para comprobar que se ente guardando en las variables la informacion ingresada desde contact
+
+        print(nombre,email,message)
+    
+    return render_template("contact.html")
+
+@app.route("/hotel")
+def hotel():
+    return render_template("hotel.html")
+
+@app.route("/services")
+def services():
+    return render_template("services.html")
+
+
+
+@app.route('/reservar', methods=["GET","POST"])
+def FormularioReserva():  
+
+    datosReserva:list = [] # puse las reservas en una lista de diccionarios, cada posicion es un diccionario que contiene la reserva
+
+    if request.method == "POST":
+
+        nombre = request.form.get("nombre_reserva")
+        cantidad_personas = request.form.get("cant_personas")
+        fecha_inicio = request.form.get("Inicio_fecha")
+        fecha_fin = request.form.get("Fin_fecha")
+    
+        reserva_id = len(datosReserva) + 1
+
+        reserva = {
+            "id": reserva_id,
+            "usuario": nombre,
+            "cantidad_personas": cantidad_personas,
+            "fecha_inicio": fecha_inicio,
+            "fecha_fin": fecha_fin
+        }
+
+        datosReserva.append(reserva)
+
+
+ 
+    return render_template('reservar.html')
+
+
+@app.errorhandler(404)  
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def server_error(e):
+    return render_template('500.html'), 500
+
 if __name__ == '__main__':
     app.run(host="127.0.0.1", port=PORT, debug=True)
