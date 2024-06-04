9b75e31525939766e0cc67a0ccf187ad342d0508
Axel C. Lopez <lopezaxel@protonmail.com>
2024-06-04 16:52:54 -0300

    Agregar labels a inputs en contact.html
    Tambien formatear app.py

--- Front/app.py
+++ Front/app.py
@@ -4,32 +4,38 @@ PORT = 5000
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
     return render_template("index.html")
+
 
 @app.route("/blog")
 def blog():
     return render_template("blog.html")
 
+
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
 
+
 @app.route("/hotel")
 def hotel():
     return render_template("hotel.html")
+
 
 @app.route("/services")
 def services():
     return render_template("services.html")
 
 
+@app.route("/reservar", methods=["GET", "POST"])
+def Reserva():
 
-@app.route('/reservar', methods=["GET","POST"])
-def Reserva():  
-
-    datosReserva:list = [] # puse las reservas en una lista de diccionarios, cada posicion es un diccionario que contiene la reserva
+    datosReserva: list = (
+        []
+    )  # puse las reservas en una lista de diccionarios, cada posicion es un diccionario que contiene la reserva
 
     if request.method == "POST":
 
@@ -37,7 +43,7 @@ def Reserva():
         cantidad_personas = request.form.get("cant_personas")
         fecha_inicio = request.form.get("Inicio_fecha")
         fecha_fin = request.form.get("Fin_fecha")
-    
+
         reserva_id = len(datosReserva) + 1
 
         reserva = {
@@ -45,23 +51,23 @@ def Reserva():
             "usuario": nombre,
             "cantidad_personas": cantidad_personas,
             "fecha_inicio": fecha_inicio,
-            "fecha_fin": fecha_fin
+            "fecha_fin": fecha_fin,
         }
 
         datosReserva.append(reserva)
 
-
- 
-    return render_template('reservar.html')
+    return render_template("reservar.html")
 
 
-@app.errorhandler(404)  
+@app.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html'), 404
+    return render_template("404.html"), 404
+
 
 @app.errorhandler(500)
 def server_error(e):
-    return render_template('500.html'), 500
+    return render_template("500.html"), 500
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(host="127.0.0.1", port=PORT, debug=True)
