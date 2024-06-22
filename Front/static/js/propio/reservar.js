ae58594b7cdfff9e713307d7142096e7e32e1faa
Axel C. Lopez <aclopez@fi.uba.ar>
2024-06-22 15:21:26 -0300

    Mejorar el mensaje de validacion en el formulario de disponibilidad

--- Front/static/js/propio/reservar.js
+++ Front/static/js/propio/reservar.js
@@ -1,14 +1,11 @@
-// al final DOMContentLoaded es mejor que load
 document.addEventListener("DOMContentLoaded", function () {
     const btnReservar = document.getElementById("btn_reservar");
 
-    // Evento que se ejecuta cuando se clickea el boton con id de "btn_reservar"
+    if (chequear) {
+        alert("Ingrese una fecha valida");
+    }
+
     btnReservar.addEventListener("click", function (event) {
         // event.preventDefault(); // Prevenir el envío del formulario
-
-        // Aquí podrías agregar el código para enviar los datos del formulario al servidor si es necesario
-
-        // Mostrar un mensaje de confirmación
-        // alert("Reserva realizada exitosamente");
     });
 });
