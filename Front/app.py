8a99348d51aa4ea9ed2b2f437497936e646ec327
Axel C. Lopez <aclopez@fi.uba.ar>
2024-06-27 11:03:54 -0300

    Refactorizar codigo app.py

--- Front/app.py
+++ Front/app.py
@@ -18,7 +18,7 @@ def home():
     return render_template("home.html"), 200
 
 
-@app.route("/contacto", methods=["GET", "POST"])
+@app.route("/contacto")
 def contacto():
     return render_template("contacto.html"), 200
 
@@ -35,56 +35,56 @@ def servicios():
 
 @app.route("/reservar_habitacion", methods=["GET", "POST"])
 def reservar_habitacion():
-    if request.method == "POST":
-        nombre = request.form.get("nombre")
-        dni = request.form.get("dni")
-        email = request.form.get("email")
-        telefono = request.form.get("telefono")
-        fecha_inicio = request.form.get("inicio_fecha")
-        fecha_fin = request.form.get("fin_fecha")
-        tipo_habitacion = request.form.get("tipo_habitacion")
-        id_habitacion = request.form.get("id_habitacion")
+    if request.method == "GET":
+        return render_template("reservar.html"), 200
 
-        datos_persona = {
-            "nombre_persona": nombre,
-            "dni_persona": dni,
-            "email_persona": email,
-            "telefono_persona": telefono,
-        }
+    nombre = request.form.get("nombre")
+    dni = request.form.get("dni")
+    email = request.form.get("email")
+    telefono = request.form.get("telefono")
+    fecha_inicio = request.form.get("inicio_fecha")
+    fecha_fin = request.form.get("fin_fecha")
+    tipo_habitacion = request.form.get("tipo_habitacion")
+    id_habitacion = request.form.get("id_habitacion")
+
+    datos_persona = {
+        "nombre_persona": nombre,
+        "dni_persona": dni,
+        "email_persona": email,
+        "telefono_persona": telefono,
+    }
+
+    QUERY = f"{BACKEND_URL}/clientes_dni/{dni}"
+    response = requests.get(QUERY)
+
+    # Si hay un error con la base de datos mostramos las reservas con su dni
+    if response.status_code == 500:
+        return reservas(dni)
+
+    # Si no esta cargado el cliente (response es un objeto con HTTP
+    # code de 404) en el sistema lo cargamos
+    if response.status_code == 404:
+        QUERY = f"{BACKEND_URL}/cargar_clientes"
+        requests.post(QUERY, json=datos_persona)
 
         QUERY = f"{BACKEND_URL}/clientes_dni/{dni}"
         response = requests.get(QUERY)
 
-        # Si hay un error con la base de datos mostramos las reservas con su dni
-        if response.status_code == 500:
-            return reservas(dni)
+    info_cliente = response.json()
+    id_cliente = info_cliente["id_persona"]
 
-        # Si no esta cargado el cliente (response es un objeto con HTTP
-        # code de 404) en el sistema lo cargamos
-        if response.status_code == 404:
-            QUERY = f"{BACKEND_URL}/cargar_clientes"
-            requests.post(QUERY, json=datos_persona)
+    tabla_reservas = {
+        "fecha_inicio": fecha_inicio,
+        "fecha_salida": fecha_fin,
+        "tipo_habitacion": tipo_habitacion,
+        "id_personas": id_cliente,
+        "id_habitaciones": id_habitacion,
+    }
 
-            QUERY = f"{BACKEND_URL}/clientes_dni/{dni}"
-            response = requests.get(QUERY)
+    QUERY = f"{BACKEND_URL}/cargar_reserva"
+    requests.post(QUERY, json=tabla_reservas)
 
-        info_cliente = response.json()
-        id_cliente = info_cliente["id_persona"]
-
-        tabla_reservas = {
-            "fecha_inicio": fecha_inicio,
-            "fecha_salida": fecha_fin,
-            "tipo_habitacion": tipo_habitacion,
-            "id_personas": id_cliente,
-            "id_habitaciones": id_habitacion,
-        }
-
-        QUERY = f"{BACKEND_URL}/cargar_reserva"
-        requests.post(QUERY, json=tabla_reservas)
-
-        return redirect(url_for("reservas_por_dni", dni=dni)), 301
-
-    return render_template("reservar.html"), 200
+    return redirect(url_for("reservas_por_dni", dni=dni)), 301
 
 
 @app.route("/reservas/<id_reserva>/<dni>", methods=["POST"])
@@ -101,7 +101,7 @@ def formatear_fecha(fecha):
     return datetime.strptime(fecha, "%a, %d %b %Y %H:%M:%S %Z").strftime("%Y-%m-%d")
 
 
-@app.route("/reservas_por_dni/<dni>", methods=["GET"])
+@app.route("/reservas_por_dni/<dni>")
 def reservas_por_dni(dni):
     QUERY = f"{BACKEND_URL}/reserva_dni/{dni}"
     response = requests.get(QUERY)
@@ -138,43 +138,43 @@ def reservas():
 
 @app.route("/reservar", methods=["GET", "POST"])
 def reservar():
-    if request.method == "POST":
-        cantidad_personas = request.form.get("cantidad_personas")
-        fecha_inicio = request.form.get("inicio_fecha")
-        fecha_fin = request.form.get("fin_fecha")
-        # Conseguimos la fecha actual utilizando la libreria date
-        fecha_actual = str(date.today())
+    if request.method == "GET":
+        return render_template("reservar.html"), 200
 
-        # Las fechas de inicio y fin son invalidas si se cumple alguna de las
-        # siguientes condiciones
-        if (
-            (fecha_inicio < fecha_actual)
-            or (fecha_fin < fecha_actual)
-            or (fecha_fin < fecha_inicio)
-        ):
-            chequear = True
-            return render_template("reservar.html", chequear=chequear), 200
+    cantidad_personas = request.form.get("cantidad_personas")
+    fecha_inicio = request.form.get("inicio_fecha")
+    fecha_fin = request.form.get("fin_fecha")
+    # Conseguimos la fecha actual utilizando la libreria date
+    fecha_actual = str(date.today())
 
-        QUERY = f"{BACKEND_URL}/mostrar_habitaciones_disponibles/{fecha_inicio}/{fecha_fin}/{cantidad_personas}"
-        response = requests.get(QUERY)
-        # Si hay un error con la base de datos utilizamos como habitaciones
-        # disponibles una lista vacia
-        if response.status_code == 500:
-            habitaciones_disponibles = []
-        else:
-            habitaciones_disponibles = response.json()
+    # Las fechas de inicio y fin son invalidas si se cumple alguna de las
+    # siguientes condiciones
+    if (
+        (fecha_inicio < fecha_actual)
+        or (fecha_fin < fecha_actual)
+        or (fecha_fin < fecha_inicio)
+    ):
+        chequear = True
+        return render_template("reservar.html", chequear=chequear), 200
 
-        return (
-            render_template(
-                "disponibilidad.html",
-                habitaciones=habitaciones_disponibles,
-                fecha_inicio=fecha_inicio,
-                fecha_fin=fecha_fin,
-            ),
-            200,
-        )
+    QUERY = f"{BACKEND_URL}/mostrar_habitaciones_disponibles/{fecha_inicio}/{fecha_fin}/{cantidad_personas}"
+    response = requests.get(QUERY)
+    # Si hay un error con la base de datos utilizamos como habitaciones
+    # disponibles una lista vacia
+    if response.status_code == 500:
+        habitaciones_disponibles = []
+    else:
+        habitaciones_disponibles = response.json()
 
-    return render_template("reservar.html"), 200
+    return (
+        render_template(
+            "disponibilidad.html",
+            habitaciones=habitaciones_disponibles,
+            fecha_inicio=fecha_inicio,
+            fecha_fin=fecha_fin,
+        ),
+        200,
+    )
 
 
 @app.route(
