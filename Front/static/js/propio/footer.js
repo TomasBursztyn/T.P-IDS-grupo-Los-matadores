8f74a70be9b034b16e66391d34521136690cfc79
Axel C. Lopez <aclopez@fi.uba.ar>
2024-06-27 11:08:46 -0300

    Vaciar input email footer con js al enviarse

--- Front/static/js/propio/footer.js
+++ Front/static/js/propio/footer.js
@@ -1,9 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
     const formularioSubscripcion = document.getElementById("form-subscribe");
+    const formEmailInput = document.getElementById("footer-email-input");
 
     formularioSubscripcion.addEventListener("submit", function (event) {
         // Prevenir el envío del formulario
         event.preventDefault();
+
+        formEmailInput.value = "";
 
         alert("Se ha registrado su email correctamente!");
     });
