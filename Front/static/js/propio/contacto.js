32b86f632b1d55d27f205e1ff3fff374e792f8be
Axel C. Lopez <aclopez@fi.uba.ar>
2024-06-27 10:03:24 -0300

    Hacer que el formulario de contacto se vacie luego de ser enviado

--- Front/static/js/propio/contacto.js
+++ Front/static/js/propio/contacto.js
@@ -1,10 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
     const btnContacto = document.getElementById("btn_enviar_contacto");
+    const inputNombre = document.getElementById("contacto_nombre");
+    const inputEmail = document.getElementById("contacto_email");
+    const inputMensaje = document.getElementById("contacto_mensaje");
 
     // Evento que se ejecuta cuando se clickea el boton con id de "btn_enviar_contacto"
     btnContacto.addEventListener("click", function (event) {
         // Prevenir el envío del formulario
         event.preventDefault();
+
+        // Vaciamos los valores de los inputs
+        inputNombre.value = "";
+        inputEmail.value = "";
+        inputMensaje.value = "";
 
         // Mostrar un mensaje de confirmación
         alert("Mensaje enviado correctamente");
